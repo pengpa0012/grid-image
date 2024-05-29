77b73e226887349ca9ffdf97c86bcd2442d90496
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-29 19:24:23 +0800

    added img pieces drag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let imgInput, img, images = []
+let imgInput, img, images = [], dragging
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -8,27 +8,54 @@ function setup() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
-  image(img, (width / 2) - (img.width / 2), (height / 2) - (img.height / 2))
 }
 
 function draw() {
   clear()
   if(images.length > 0) {
     for(let i = 0; i < images.length; i++) {
-      image(images[i], i * images[i].width, height - images[i].height - 100)
+      if (images[i].dragging) {
+        images[i].posX = mouseX + images[i].offsetX
+        images[i].posY = mouseY + images[i].offsetY
+      }
+      image(images[i], images[i].posX, images[i].posY)
     }
+  }
+}
+
+
+function mousePressed() {
+  if(images.length > 0) {
+    for(let i = 0; i < images.length; i++) {
+      if (
+        mouseX > images[i].posX && 
+        mouseX < images[i].posX + images[i].width && 
+        mouseY > images[i].posY && 
+        mouseY < images[i].posY + images[i].height
+      ) {
+        images[i].dragging = true
+        images[i].offsetX = images[i].posX - mouseX
+        images[i].offsetY = images[i].posY - mouseY
+      }
+    }
+  }
+}
+
+function mouseReleased() {
+  for(let i = 0; i < images.length; i++) {
+    images[i].dragging = false
   }
 }
 
 function onHandleImage(file) {
   if(file.type == "image") {
-    // imgInput.class("hide")
+    imgInput.class("hide")
     images = []
     loadImage(file.data, e => {
       img = e
       img.width = 500
       img.height = 500
-      images = sliceImages(img, 3)
+      images = sliceImages(img, 5)
     })
   } else {
     imgInput.elt.value = ""
@@ -45,7 +72,9 @@ function sliceImages(img, dimension) {
       const x = j * cellSize
       const y = i * cellSize
       let image = img.get(x, y, cellSize, cellSize)
-      imgPieces.push(image)
+      const posX = random(0, width - img.width)
+      const posY = random(0, height - (img.height - 100))
+      imgPieces.push({...image, posX, posY})
     }
   }
   return imgPieces
