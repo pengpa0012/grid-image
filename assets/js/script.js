e27988f4fc7eb8694cea48adb53127bb3852a42f
Pengpa0012 <peng.ilikha@gmail.com>
2024-05-29 18:32:23 +0800

    added slice image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let imgInput, img
+let imgInput, img, images = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
@@ -6,14 +6,47 @@ function setup() {
   imgInput.position(width/2, height/2)
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+  image(img, (width / 2) - (img.width / 2), (height / 2) - (img.height / 2))
+}
+
+function draw() {
+  clear()
+  if(images.length > 0) {
+    for(let i = 0; i < images.length; i++) {
+      image(images[i], i * images[i].width, height - images[i].height - 100)
+    }
+  }
+}
+
 function onHandleImage(file) {
   if(file.type == "image") {
-    imgInput.class("hide")
+    // imgInput.class("hide")
+    images = []
     loadImage(file.data, e => {
-      img = image(e, (width/2) - (e.width / 2), (height/2) - (e.height / 2))
+      img = e
+      img.width = 500
+      img.height = 500
+      images = sliceImages(img, 3)
     })
   } else {
     imgInput.elt.value = ""
     alert("Wrong file type")
   }
+}
+
+// generate img pieces
+function sliceImages(img, dimension) {
+  let imgPieces = []
+  const cellSize = img.width / dimension
+  for(let i = 0; i < dimension; i++) {
+    for(let j = 0; j < dimension; j++) {
+      const x = j * cellSize
+      const y = i * cellSize
+      let image = img.get(x, y, cellSize, cellSize)
+      imgPieces.push(image)
+    }
+  }
+  return imgPieces
 }
